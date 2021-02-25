9f2ee84d042dadeae772ebde24fabf0771c4fdf2
Vinay Matta <vinaymatta63@gmail.com>
2021-02-25 22:19:47 +0530

    Deploy

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyCZCKHQU4FryJvdbe6MUILbDz5-xoINxSI",
+  authDomain: "clone-backend-24946.firebaseapp.com",
+  projectId: "clone-backend-24946",
+  storageBucket: "clone-backend-24946.appspot.com",
+  messagingSenderId: "1015840224389",
+  appId: "1:1015840224389:web:9bd98b4734d8a3b52e25ec",
+  measurementId: "G-7Y1P9WFLE9",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
